b2a0963cdeeb82e4a16c12d21c6cf5374592e0be
Gabriel Pereyra <gabrielp@unitech.com.ar>
2023-01-14 17:11:00 -0300

    Json pipe added

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,50 +1,55 @@
 import { ThisReceiver } from '@angular/compiler';
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styleUrls: ['./no-comunes.component.css']
+  styleUrls: ['./no-comunes.component.css'],
 })
-export class NoComunesComponent{
+export class NoComunesComponent {
   //i18Select
-  public nombre : string = 'Gabriel';
-  public genero : string = 'masculino';
+  public nombre: string = 'Gabriel';
+  public genero: string = 'masculino';
 
-  public invitacionMapping ={
-    'masculino' : 'tenerlo',
-    'femenino' : 'tenerla',
-    'other': ''
-  }
+  public invitacionMapping = {
+    masculino: 'tenerlo',
+    femenino: 'tenerla',
+    other: '',
+  };
 
-  cambiarPersona(){
-    if(this.genero === 'masculino'){
+  cambiarPersona() {
+    if (this.genero === 'masculino') {
       this.genero = 'femenino';
       this.nombre = 'Sofia';
-    } else{
+    } else {
       this.genero = 'masculino';
       this.nombre = 'Gabriel';
     }
   }
 
-   //i18Plural
-   public clientes : string[] = ["Gabriel", "Sofia", "Jose","Maria"];
-   public clientesMapping={
+  //i18Plural
+  public clientes: string[] = ['Gabriel', 'Sofia', 'Jose', 'Maria'];
+  public clientesMapping = {
     '=0': 'no existen clientes esperando.',
     '=1': 'existe 1 cliente esperando.',
-    'other': 'existen # clientes esperando'
-   }
+    other: 'existen # clientes esperando',
+  };
 
-   borrarCliente(){
-      this.clientes.pop();
-   }
-      
-   //keyValuePipe
-   public persona= {
-    nombre: "Gabriel",
+  borrarCliente() {
+    this.clientes.pop();
+  }
+
+  //keyValuePipe
+  public persona = {
+    nombre: 'Gabriel',
     edad: 33,
-    pais: "Argentina",
-   }
+    pais: 'Argentina',
+  };
 
-  
+  //Json pipe
+  public jsonValue = [
+    { heroe: 'Superman', habilidad: 'Super fuerza' },
+    { heroe: 'Spiderman', habilidad: 'Sentido aracnido' },
+    { heroe: 'Flash', habilidad: 'Super velocidad' },
+  ];
 }
